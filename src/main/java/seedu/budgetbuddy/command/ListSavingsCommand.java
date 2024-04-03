288cc7c72af8d778c491c53fe4d01ad2d9471723
Zhang Yangda <zhang.yangda16@gmail.com>
2024-04-03 21:38:12 +0800

    Add Comments

--- src/main/java/seedu/budgetbuddy/command/ListSavingsCommand.java
+++ src/main/java/seedu/budgetbuddy/command/ListSavingsCommand.java
@@ -8,16 +8,33 @@ public class ListSavingsCommand extends Command {
     private ExpenseList expenses;
     private String filterCategory;
 
+    /**
+     * Creates a ListSavingsCommand object.
+     * 
+     * @param savings The list of savings.
+     * @param expenses The list of expenses.
+     */
     public ListSavingsCommand(SavingList savings, ExpenseList expenses) {
         this.savings = savings;
         this.expenses = expenses;
     }
 
+    /**
+     * Creates a ListSavingsCommand object.
+     * 
+     * @param savings The list of savings.
+     * @param expenses The list of expenses.
+     * @param filterCategory The category to filter the savings by.
+     */
     public ListSavingsCommand(SavingList savings, ExpenseList expenses, String filterCategory) {
         this.savings = savings;
         this.expenses = expenses;
         this.filterCategory = filterCategory;
     }
+
+    /**
+     * Lists all the savings.
+     */
 
     @Override
     public void execute() {
--- src/main/java/seedu/budgetbuddy/command/ListSplitExpenseCommand.java
+++ src/main/java/seedu/budgetbuddy/command/ListSplitExpenseCommand.java
@@ -5,10 +5,19 @@ import seedu.budgetbuddy.commons.SplitExpenseList;
 public class ListSplitExpenseCommand extends Command{
     private SplitExpenseList splitexpenses;
 
+    /**
+     * Creates a ListSplitExpenseCommand object.
+     * 
+     * @param splitexpenses The list of split expenses.
+     */
+
     public ListSplitExpenseCommand(SplitExpenseList splitexpenses) {
         this.splitexpenses = splitexpenses;
     }
 
+    /**
+     * Lists all the split expenses.
+     */
     @Override
     public void execute() {
         splitexpenses.listSplitExpenses();
