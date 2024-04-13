f6844aedeadb11e4b5959bf11babd76fde21ae2c
Zhang Yangda <zhang.yangda16@gmail.com>
2024-04-13 16:52:04 +0800

    Revert back the changes for filterExpense

--- src/main/java/seedu/budgetbuddy/commons/ExpenseList.java
+++ src/main/java/seedu/budgetbuddy/commons/ExpenseList.java
@@ -77,9 +77,9 @@ public class ExpenseList {
         String descriptionInLowerCase = description.toLowerCase();
         ArrayList<Expense> filteredExpenses = new ArrayList<>(this.expenses.stream()
                 .filter(expense -> (expense.getDescription()
-                        .toLowerCase().contains(descriptionInLowerCase)))
-                .filter(expense -> (minAmount == null || expense.getAmount() > minAmount))
-                .filter(expense -> (maxAmount == null || expense.getAmount() < maxAmount))
+                .toLowerCase().contains(descriptionInLowerCase)))
+                .filter(expense -> (minAmount == null || expense.getAmount() >= minAmount))
+                .filter(expense -> (maxAmount == null || expense.getAmount() <= maxAmount))
                 .collect(Collectors.toList()));
 
         LOGGER.log(Level.INFO, "Ending filtering and returning filtered expenses");
